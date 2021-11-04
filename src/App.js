300b0d4c1aaeeec8e8e20e9649456e2d4f6bfa47
Dmitri Lazovschi <dmitri.lazovschi@icloud.com>
2021-11-04 22:53:52 +0100

    jenkins test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{process.env.REACT_APP_NAME}</p>
-        <p>Hi</p>
+        <p>Hi von jenkins</p>
         <p>trest</p>
         <a
           className="App-link"
