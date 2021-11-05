05cfcacdfae76bb1309495a4b57040005fb45474
Dmitri Lazovschi <dmitri.lazovschi@icloud.com>
2021-11-05 22:25:29 +0100

    new

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{process.env.REACT_APP_NAME}</p>
-        <p>Hi von jenkins</p>
+        <p>Hi von jenkins New </p>
         <p>trest</p>
         <a
           className="App-link"
